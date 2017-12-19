29a32c91a4ba4957df77ae973cbac94b097c8dc1
Liam Houghton <liamhoughton90@yahoo.com>
2017-12-19 10:03:23 -0600

    Completed Pay Calculator

---
+++ PayCalculatorEx/PayCalculator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayCalculatorEx
+{
+    class PayCalculator
+    {
+        string hours;
+        string rate;
+
+        private string DisplayHours()
+        {
+            Console.WriteLine("Plase enter your hours");
+            string numberHours = Console.ReadLine();
+            /*Console.ReadKey();*/
+            return numberHours;
+        }
+        private string Displayrate()
+        {
+            Console.WriteLine("Please enter your rate");
+            string howMuchPaid = Console.ReadLine();
+            /*Console.ReadKey();*/
+            return howMuchPaid;
+        }
+        public float CalculatePay()
+        {
+            hours = DisplayHours();
+            rate = Displayrate();
+
+            float hoursTotal = float.Parse(hours);
+            float rateTotal = float.Parse(rate);
+            float pay = hoursTotal * rateTotal;
+            return pay;
+
+        }
+        public void DisplayResults()
+        {
+          float results = CalculatePay();
+            Console.WriteLine("Your pay is: {0}, good job", results);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PayCalculatorEx/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayCalculatorEx
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PayCalculator pay = new PayCalculator();
+            pay.DisplayResults();
+            Console.ReadKey();
+            
+            
+            /*hours = Console.ReadLine();
+            rate = Console.ReadLine();*/
+            
+           
+            
+
+          
+
+        }
+    }
+}
